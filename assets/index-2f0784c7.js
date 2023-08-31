e86fe6b5d0f300f645bf61c061a11f863dc3ea32
xuzhen <42487725+uxzhen@users.noreply.github.com>
2023-08-31 15:08:18 +0800

    Add files via upload

---
+++ assets/index-2f0784c7.js
@@ -0,0 +1,1 @@
+import{g as s}from"./fileType-5409012c.js";import{d as t,c as _,f as n,g as o,o as a,_ as c}from"./index-9dd562d1.js";const r=["src"],d=t({__name:"index",setup(i){const e=s();return(l,p)=>(a(),_("div",null,[n("img",{src:o(e),alt:"img"},null,8,r)]))}});const u=c(d,[["__scopeId","data-v-8fb91a8e"]]);export{u as default};
